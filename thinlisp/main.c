e60c89eeb0fb50c9da8e94406a3794a6c5708f3d
Eric Anderson <ericjame@gmail.com>
2016-09-11 10:16:12 -0400

    Initial Commit

---
+++ thinlisp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  thinlisp
+//
+//  Created by Eric on 2016-09-11.
+//  Copyright © 2016 norg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
